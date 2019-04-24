4ecbe83ed646341d3c6302e139e4bcb39b70141c
Nick Schoch <nschoch@gmail.com>
2019-04-24 18:09:20 +0000

    completed book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,35 @@
 class Book
 # write your code here
+    def initialize()
+        @title = title
+    end
+
+    def title
+        titleize(@title.to_s)
+    end
+
+    def title=(text)
+        @title = text.to_s
+    end
+
+end
+
+def titleize(text)
+    textArray = text.split(' ')
+    newString = ''
+    counter = 0
+
+    textArray.each do |word|
+        counter += 1
+        if(word != 'the' && word != 'of' && word != 'for' && 
+           word != 'and' && word != 'over' && word!= 'in' &&
+           word != 'a' && word != 'an')
+            newString += word.capitalize + ' '
+        elsif (counter == 1)
+            newString += word.capitalize + ' '
+        else
+            newString += word + ' '
+        end
+    end
+    newString.strip!
 end
