79e2188dcf1e3833ae3de6c87eb82c644cad026a
Nick Schoch <nschoch@gmail.com>
2019-04-24 17:56:20 +0000

    pig latin exercise complete

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,27 @@
 #write your code here
+
+def translate(phrase)
+  translated_phrase = ''
+  phrase_array = phrase.split(' ')
+
+  phrase_array.each do |word|
+    translated_word = ''
+    first_vowel = word.index(/[aeiou]/)
+    qu_location = word.downcase.index('qu')
+
+    if (qu_location)
+      translated_word += word[qu_location+2..] + word[0..qu_location+1]
+    else
+      translated_word += word[first_vowel..]
+      
+      if (first_vowel != 0)
+        translated_word += word[0..first_vowel-1]
+      end
+    end
+    translated_word += 'ay'
+    translated_phrase += translated_word + ' '
+  end
+
+  translated_phrase.strip
+
+end
